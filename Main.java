02359d928ecb0b92404bc29b1ea8d4a1e35891a7
ttdangg <ttdang1445@gmail.com>
2024-11-04 10:16:13 -0800

    #1 and #2

--- Main.java
+++ Main.java
@@ -4,6 +4,45 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    // int num = 0;
+    // int sum = 0;
+
+    // Problem 1
+    // System.out.println("Enter any numbers (Enter -1 to stop): ");
+    
+    // while (num != -1)
+    // {
+    //   num = sc.nextInt();
+    //   sum += num ;
+    // }
+    // sum ++;
+    // System.out.println("Sum: " + sum);
+
+
+    // Problem 2
+    boolean firstNum = true;
+    int max = Integer.MIN_VALUE; 
+
+    int input = 0;
+
+    System.out.println("How many numbers do you want to input: ");
+    int count = sc.nextInt(); 
+
+    System.out.println("Enter the scores: ");
+    int i = 0; 
+
+    while (i < count)
+    {
+      if (firstNum || input > max){
+        max = input;
+        firstNum = false;
+      }
+
+      i++;
+    }
+    System.out.println("The max is " + max);
+
+
   }
 }
